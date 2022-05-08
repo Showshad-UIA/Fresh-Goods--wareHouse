f9e8751f104d139aadc1df758bd5dde4ed2e4ca5
raihan <raihanulmcse@mail.com>
2022-05-08 23:33:13 +0800

    banner added

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -3,7 +3,7 @@ import React from "react";
 const Blog = () => {
 	return (
 		<div className="mt-5">
-			<div className="container"></div>
+			<div className="container">Question 1:</div>
 
 			<div className="container"></div>
 			<div className="container"></div>
