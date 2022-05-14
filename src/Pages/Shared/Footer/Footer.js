42a55507dcc16a22b00635ce036182f9e9a047c3
raihan <raihanulmcse@mail.com>
2022-05-14 23:00:31 +0800

    Footer being added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,22 +1,61 @@
 import React from "react";
+import {
+	Box,
+	Container,
+	Row,
+	Column,
+	FooterLink,
+	Heading,
+} from "./FooterStyles";
 
 const Footer = () => {
 	return (
-		<div className="footer">
-			<footer className="text-center mt-5 bg-dark text-white">
-				<p>Level-1, 24, Sri Gombak, IIUM, Malaysia</p>
-				<p> Official:FreshGoods@gmail.com</p>
-				<p>
-					Phone: +601201701701 , +601521801101 About us Copyright © 2022 Fresh
-					Goods.com
-				</p>
+		<Box>
+			<Container>
+				<Row>
+					<Column>
+						<Heading>About Us</Heading>
+						<FooterLink href="#">Supplier</FooterLink>
+						<FooterLink href="#">Origin</FooterLink>
+						<FooterLink href="#">Stock</FooterLink>
+					</Column>
+					<Column>
+						<Heading>Services</Heading>
+						<FooterLink href="#">Wholesale</FooterLink>
+						<FooterLink href="#">Exchange</FooterLink>
+						<FooterLink href="#">Export</FooterLink>
+					</Column>
+					<Column>
+						<Heading>Contact Us</Heading>
+						<FooterLink href="#">Klang</FooterLink>
+						<FooterLink href="#">Penang</FooterLink>
+						<FooterLink href="#">Kualalumpur</FooterLink>
+					</Column>
+					<Column>
+						<Heading>Social Media</Heading>
+						<FooterLink href="#">
+							<i className="fab fa-facebook-f">
+								<span style={{ marginLeft: "10px" }}>Facebook</span>
+							</i>
+						</FooterLink>
+						<FooterLink href="#">
+							<i className="fab fa-instagram">
+								<span style={{ marginLeft: "10px" }}>Instagram</span>
+							</i>
+						</FooterLink>
 
-				<p>
-					<small>copyright@Gadget House</small>
-				</p>
-			</footer>
-		</div>
+						<FooterLink href="#">
+							<i className="fab fa-youtube">
+								<span style={{ marginLeft: "10px" }}>Youtube</span>
+							</i>
+						</FooterLink>
+					</Column>
+				</Row>
+				<h4 className="text-center mt-3 text-white">
+					Copyright@fresh-Goods-Warehouse
+				</h4>
+			</Container>
+		</Box>
 	);
 };
-
 export default Footer;
