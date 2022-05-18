6407702fab856596dc89cd769264bd3e39584842
raihan <raihanulmcse@mail.com>
2022-05-18 23:39:29 +0800

    code has edited

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -5,7 +5,7 @@ const Blog = () => {
 	return (
 		<div className="container mt-5 mb-5">
 			<div>
-				Question 1: What is Different JavaScript and Nodejs? JavaScript is
+				Question-1: What is Different JavaScript and Nodejs? JavaScript is
 				programming languages and writing scripts on the website. Nodejs is a
 				JavaScript runtime environment.JavaScript used on the client
 				side,frontend development and nodeJs used on the server side
@@ -14,12 +14,12 @@ const Blog = () => {
 			</div>
 
 			<div>
-				Question 2: When used Nodejs or Mongodb? Nodejs is a JavaScript runtime
+				Question-2: When used Nodejs or Mongodb? Nodejs is a JavaScript runtime
 				environment.Actually nodejs used when we develop server site. And
 				Mongodb doing work store data. So, mongodb used for data store.
 			</div>
 			<div>
-				Question 3: What is Different SQL and NoSQL? SQL Data structure is
+				Question-3: What is Different SQL and NoSQL? SQL Data structure is
 				relational or NoSQL is no relational data structure.SQL databases
 				defines and manipulates data based structured query language.NoSQL is
 				database dynamic schema for unstructured data.SQL vertically scalable ,
